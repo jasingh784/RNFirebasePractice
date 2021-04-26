fd585b049746b6b521f5486780ca9c2b2cfd886b
jasingh784 <48878742+jasingh784@users.noreply.github.com>
2021-04-25 20:40:58 -0700

    just practice

---
+++ src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function HomeScreen(props) {
+    console.log(props)
+    return (
+        <View>
+            <Text>This is the home screen </Text>
+        </View>
+    )
+}
